dec61c8852585c501e088b2f11798ce23bed0e55
satyaba <satyaba7@gmail.com>
2021-09-10 15:54:32 +0700

    first commit

---
+++ src/pages/Blog.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+    <p>Tes blog</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Blogging",
+  },
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ src/pages/Docs.vue
@@ -0,0 +1,13 @@
+<template>
+  <Layout>
+    <h1>Newly created pages by Bayu Satya.</h1>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Docs",
+  },
+};
+</script>
